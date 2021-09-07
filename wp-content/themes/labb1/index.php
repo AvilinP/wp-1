036cb63363d2ec62553a90882ae647e19a0a6e54
Avilin <avilin.persson@gmail.com>
2021-09-07 15:19:59 +0200

    added all pages in menu

--- wp-content/themes/labb1/index.php
+++ wp-content/themes/labb1/index.php
@@ -11,7 +11,6 @@
 						<div class="col-xs-12">
 							<div class="hero">
                                 <img src="<?php echo get_home_url(); ?>/wp-content/uploads/2021/09/city.jpg" />
-								<!-- <img src="labb1/img/city.jpg" /> -->
 								<div class="text">
 									<h1>Hej och välkommen!</h1>
 									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed sodales mauris. Aliquam felis est, efficitur vel fringilla quis, vehicula quis ex.</p>
