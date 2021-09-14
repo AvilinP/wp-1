7c2b4d1719f1c3636d8950eb13f582d14e175dd6
Avilin <avilin.persson@gmail.com>
2021-09-14 13:56:47 +0200

    fixed sidebar on category

--- wp-content/themes/labb1/category.php
+++ wp-content/themes/labb1/category.php
@@ -40,87 +40,15 @@
 										</article>
  
                     				<?php endwhile; ?>
-        						</div>
+        						
     					
 							<nav class="navigation pagination">
 								<h2 class="screen-reader-text">Inläggsnavigering</h2>
 								<?php echo paginate_links(); ?>
 							</nav>
-						</div>
-
 						<?php endif; ?> 
-
-
-
-				
-						<aside id="secondary" class="col-xs-12 col-md-3">
-							<div id="sidebar">
-								<ul>
-									<li>
-										<form id="searchform" class="searchform">
-											<div>
-												<label class="screen-reader-text">Sök efter:</label>
-												<input type="text" />
-												<input type="submit" value="Sök" />
-											</div>
-										</form>
-									</li>
-								</ul>
-								<ul role="navigation">
-									<li class="pagenav">
-										<h2>Sidor</h2>
-										<ul>
-											<li class="page_item current_page_item">
-												<a href="">Blogg</a>
-											</li>
-											<li class="page_item">
-												<a href="">Exempelsida</a>
-											</li>
-											<li class="page_item">
-												<a href="">Kontakt</a>
-											</li>
-											<li class="page_item page_item_has_children">
-												<a href="">Om mig</a>
-												<ul class="children">
-													<li class="page_item">
-														<a href="">Intressen</a>
-													</li>
-													<li class="page_item page_item_has_children">
-														<a href="">Portfolio</a>
-														<ul class="children">
-															<li class="page_item">
-																<a href="">Projekt 1</a>
-															</li>
-														</ul>
-													</li>
-												</ul>
-											</li>
-											<li class="page_item">
-												<a href="">Startsida</a>
-											</li>
-										</ul>
-									</li>
-									<li>
-										<h2>Arkiv</h2>
-										<?php wp_get_archives();?>
-												<li class="categories">
-												<h2>Kategorier</h2>
-												<ul>
-												<?php
-												$categories = get_categories( array(
-													'orderby' => 'name',
-													'order'   => 'ASC'
-												) );
-
-												foreach( $categories as $category ) {
-												echo '<li class="cat-item"><a href="' . get_category_link($category->term_id) . '">' . $category->name . '</a><span class="text-white"> (' . $category->category_count . ')' . '</span></li>';   
-												} ?>
-												</ul>
-											</li>
-
-								</ul>
-							</div>
-						</aside>
+						</div>
+							<?php get_sidebar('side-bar'); ?>		
 					</div>
 				</div>
 			</section>
